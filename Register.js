aebcc8c6ff1214e478b5c3a2543219fd24d739ba
Soumodeep Halder <106613034+SoumodeepCodeOP@users.noreply.github.com>
2023-07-09 16:24:04 +0530

    Update Register.js

--- Register.js
+++ Register.js
@@ -9,14 +9,13 @@ submitbtn.onclick = () =>{
 
 
     let xhr = new XMLHttpRequest();    
-    xhr.open("POST","./Php/Register.php",true);
+    xhr.open("POST","./Register.php",true);
     xhr.onload = () => {
         if(xhr.readyState === XMLHttpRequest.DONE){
             if(xhr.status == 200){
                 let data = xhr.response;
                 if(data=="success"){
-                    alert("Registration succesfull");
-                    location.href="";
+                    location.href="Verify.html";
                 }
                 else{
                     errortxt.textContent = data;
