6e102dd3a2e11fb6e83d81bcb47d300b1d437e6f
Soumodeep Halder <106613034+SoumodeepCodeOP@users.noreply.github.com>
2023-07-09 16:25:45 +0530

    Update otp.php

--- otp.php
+++ otp.php
@@ -13,7 +13,7 @@ $otp = $otp1.$otp2.$otp3.$otp4;
 
 if(!empty($otp)){
     if($otp == $session_otp){
-        $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id ='{$unique_id}' AND otp = '{$otp}'");
+        $sql = mysqli_query($conn, "SELECT * FROM Users WHERE unique_id ='{$unique_id}' AND otp = '{$otp}'");
         if(mysqli_num_rows($sql)>0){
             $null_otp = 0;
             $sql2 = mysqli_query($conn,"UPDATE users SET `verification_status`= 'Verified',`otp`='$null_otp' WHERE $unique_id ='{$unique_id}'");
